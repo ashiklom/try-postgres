336888e2539fd3530ba203a19fd569cb6151a92d
Alexey Shiklomanov <alexey.shiklomanov@gmail.com>
2019-05-23 10:02:13 -0400

    Update create_db script

--- create_db.R
+++ create_db.R
@@ -1,13 +1,8 @@
-requireNamespace("data.table", quietly = TRUE)
-# begin imports
-import::from("magrittr", "%>%", .into = "")
-import::from("DBI", "dbConnect", "dbCreateTable", "dbSendQuery", "dbRemoveTable", "dbAppendTable", .into = "")
-import::from("RPostgres", "Postgres", .into = "")
-import::from("dplyr", "distinct", "select", "tbl", "count", "group_by", "mutate", "filter", "collect", .into = "")
-import::from("fst", "read_fst", .into = "")
-import::from("glue", "glue", .into = "")
-# end imports
-import::from("data.table", "fwrite", .into = "")
+library(data.table)
+library(dplyr, exclude = c("between", "first", "last"))
+library(fst)
+library(glue, exclude = c("collapse"))
+library(RPostgres)
 
 con <- dbConnect(
   Postgres(),
@@ -21,11 +16,11 @@ try_file <- "~/Projects/try-raw-data/4143.fst"
 all_try <- read_fst(try_file, as.data.table = TRUE)
 
 # Assign a reference ID
-data.table::setnames(all_try, "Reference", "Citation")
+setnames(all_try, "Reference", "Citation")
 all_try <- all_try[, CitationID := .GRP, by = Citation]
 
 # Unique tables
-species <- all_try[, .N, by = .(AccSpeciesID, AccSpeciesName, SpeciesName)]
+species_raw <- all_try[, .N, by = .(AccSpeciesID, AccSpeciesName, SpeciesName)]
 datasets <- all_try[, .N, by = .(DatasetID, Dataset)]
 datatypes <- all_try[, .N, by = .(TraitID, TraitName, DataID, DataName)]
 citations <- all_try[, .N, by = .(CitationID, Citation)]
@@ -36,72 +31,77 @@ try_data <- all_try %>%
          -Citation, -V28,
          -LastName, -FirstName)
 
+# Uniqify species
+dup_species <- species_raw %>%
+  count(AccSpeciesID) %>%
+  filter(n > 1) %>%
+  inner_join(species, by = "AccSpeciesID") %>%
+  group_by(AccSpeciesID, AccSpeciesName) %>%
+  summarize(Synonyms = paste(SpeciesName, collapse = "|"),
+            N = sum(N))
+
+species <- species_raw %>%
+  anti_join(dup_species, by = "AccSpeciesID") %>%
+  rename(Synonyms = SpeciesName) %>%
+  bind_rows(dup_species) %>%
+  as_tibble()
+
 dbCreateTable(con, "species", species)
 dbCreateTable(con, "datasets", datasets)
 dbCreateTable(con, "datatypes", datatypes)
 dbCreateTable(con, "citations", citations)
 dbCreateTable(con, "trydata", try_data)
 
-dup_species <- tbl(con, "species") %>%
-  count(AccSpeciesID) %>%
-  filter(n > 1) %>%
-  collect()
-
 set_primary_key <- function(con, table, column) {
   query <- glue::glue('ALTER TABLE "{table}" ADD PRIMARY KEY ("{column}")')
-  dbSendQuery(con, query)
+  DBI::dbExecute(con, query)
+}
+
+set_foreign_key <- function(con, table, key, foreign_table,
+                            foreign_key = key,
+                            constraint = paste(table, key,
+                                               "fk", sep = "_")) {
+  query <- glue::glue_sql(
+    "ALTER TABLE {`table`} ",
+    "ADD CONSTRAINT {`constraint`} ",
+    "FOREIGN KEY ({`key`}) ",
+    "REFERENCES {`foreign_table`} ({`foreign_key`})",
+    .con = con
+  )
+  for (q in query) DBI::dbExecute(con, q)
 }
 
 set_primary_key(con, "species", "AccSpeciesID")
-set_primary_key(con, "species", "AccSpeciesID")
+set_primary_key(con, "datasets", "DatasetID")
+set_primary_key(con, "datatypes", "DataID")
+set_primary_key(con, "citations", "CitationID")
+set_primary_key(con, "trydata", "ObsDataID")
 
-dbSendQuery(con, 'ALTER TABLE species ADD PRIMARY KEY ("AccSpeciesID")')
-dbSendQuery(con, 'ALTER TABLE species ADD PRIMARY KEY ("AccSpeciesID")')
-dbSendQuery(con, paste(
-  "ALTER TABLE trydata ",
-  "ADD CONSTRAINT species_fk ",
-  'FOREIGN KEY ("AccSpeciesID") REFERENCES species ("AccSpeciesID")'
-))
-dbSendQuery(con, paste(
-  "ALTER TABLE trydata",
-  "ADD CONSTRAINT dataset_fk",
-  'FOREIGN KEY ("DatasetID") REFERENCES datasets ("DatasetID")'
-))
-dbSendQuery(con, paste(
-  "ALTER TABLE trydata",
-  "ADD CONSTRAINT trait_fk",
-  "FOREIGN KEY (TraitID, DataID) REFERENCES datatypes (TraitID, DataID)"
-))
-dbSendQuery(con, paste(
-  "ALTER TABLE trydata",
-  "ADD CONSTRAINT reference_fk",
-  "FOREIGN KEY (CitationID) REFERENCES citations (CitationID)"
-))
-dbSendQuery(con, paste(
-  "ALTER TABLE trydata",
-  "ADD PRIMARY KEY (\"ObsDataID\")"
-))
+set_foreign_key(con, "trydata", "AccSpeciesID", "species")
+set_foreign_key(con, "trydata", "DatasetID", "datasets")
+set_foreign_key(con, "trydata", "DataID", "datatypes")
+set_foreign_key(con, "trydata", "CitationID", "citations")
 
-dbAppendTable(con, "species", species)
-dbAppendTable(con, "datasets", datasets)
-dbAppendTable(con, "datatypes", datatypes)
-dbAppendTable(con, "citations", citations)
+dbWriteTable(con, "species", species, append = TRUE)
+dbWriteTable(con, "datasets", datasets, append = TRUE)
+dbWriteTable(con, "datatypes", datatypes, append = TRUE)
+dbWriteTable(con, "citations", citations, append = TRUE)
 
-temp_file <- tempfile(fileext = ".csv")
-fwrite(try_data, temp_file)
-dbSendQuery(con, glue(
-  "COPY trydata FROM {temp_file}"
-))
-dbAppendTable(con, "trydata", try_data)
+remove_invalid_utf8 <- function(x) {
+  if (!is.character(x)) return(x)
+  Encoding(x) <- "UTF-8"
+  iconv(x, "UTF-8", "UTF-8", sub = "")
+}
 
-count(try_data)
-tbl(con, "trydata")
+try_data_fixed <- try_data[, lapply(.SD, remove_invalid_utf8)]
 
+nsplit <- 100
+splitby <- rep(seq.int(1, nsplit), length.out = nrow(try_data_fixed))
+try_data_split <- split(try_data_fixed, splitby)
 
-if (FALSE) {
-  dbRemoveTable(con, "species")
-  dbRemoveTable(con, "datasets")
-  dbRemoveTable(con, "datatypes")
-  dbRemoveTable(con, "citations")
-  dbRemoveTable(con, "trydata")
+pb <- progress::progress_bar$new(total = nsplit)
+for (i in seq_along(try_data_split)) {
+  pb$tick()
+  dbWriteTable(con, "trydata", try_data_split[[i]],
+               copy = TRUE, append = TRUE)
 }
